9397b74718560986ae3ef7890d97061c0e62c73a
Daniel <daniel@zookeeper.app>
2021-11-22 15:52:26 +0100

    Created sample project

---
+++ SyncFusionLiveTest/DataPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SyncFusionLiveTest
+{
+	public class DataPoint
+	{
+		public DataPoint()
+		{
+		}
+
+		public DataPoint(DateTime x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public DateTime X { get; set; }
+
+		public double Y { get; set; }
+	}
+}
